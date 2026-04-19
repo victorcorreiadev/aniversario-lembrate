cae0f679b52dc404396780e02651e03876edef11
victordevz <victorcorreia019va@gmail.com>
2026-04-19 10:02:24 -0300

    teste

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,9 @@
+import dns from "dns";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
+
+dns.setDefaultResultOrder("ipv4first");
 
 const client = postgres(process.env.DATABASE_URL!, {
   ssl: process.env.NODE_ENV === "production" ? "require" : false,
